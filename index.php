26354e57abd983f297a464e21b768c68800771db
Arun240454 <101058635+Arun240454@users.noreply.github.com>
2025-02-19 00:50:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 	<h1>MySQL Table Viewer</h1>
 	<?php
 		// Define database connection variables
-		$servername = "DBServer";
-		$username = "DB_USER";
-		$password = "DB_PASSWORD";
-		$dbname = "DB_NAME";
+		$servername = "mysqldb-server";
+		$username = "mysuer";
+		$password = "myPassword@123";
+		$dbname = "mysql";
 
 		// Create database connection
 		$conn = new mysqli($servername, $username, $password, $dbname);
